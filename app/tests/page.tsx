03166c64499afcdcab1faf2a3b2a521a6e8dc8f8
Damien Mauchamp <damienmauchamp@gmail.com>
2024-02-26 01:18:36 -0500

    first display

--- app/tests/page.tsx
+++ app/tests/page.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import axios from 'axios'
 import Image from 'next/image'
+import { IoRefreshCircleOutline } from 'react-icons/io5'
 
 const apiHeaders = {
 	Accept: 'application/json',
@@ -68,31 +69,154 @@ async function getUpcomingSongs() {
 		},
 	})
 }
-// todo : first display
-// todo : different display (songs, albums)
+
 // todo : sections components
 
 export default function Test() {
-	const getNewReleasesHandler = async () => {
-		const res = await getNewReleases()
-		console.log('res', res)
-	}
-
-	const [newReleases, setNewReleases] = useState<any>(null)
-	const [newSingles, setNewSingles] = useState<any>(null)
-	const [upcoming, setUpcoming] = useState<any>(null)
-	const [newSongs, setNewSongs] = useState<any>(null)
-	const [upcomingSongs, setUpcomingSongs] = useState<any>(null)
+	const [newReleases, setNewReleases] = useState<any[]>([])
+	const [newSingles, setNewSingles] = useState<any[]>([])
+	const [upcoming, setUpcoming] = useState<any[]>([])
+	const [newSongs, setNewSongs] = useState<any[]>([])
+	const [upcomingSongs, setUpcomingSongs] = useState<any[]>([])
 
 	// useEffect(() => {})
 
-	// const getNewReleases = async () => {
-	// 	const res = await getNewReleases()
-	// 	setNewReleases(res.data)
-	// }
+	const handleNewReleases = async () => {
+		const res = await getNewReleases()
+		setNewReleases(res.data)
+	}
+	const handleNewSingles = async () => {
+		const res = await getNewSingles()
+		setNewSingles(res.data)
+	}
+	const handleUpcoming = async () => {
+		const res = await getUpcoming()
+		setUpcoming(res.data)
+	}
+	const handleNewSongs = async () => {
+		const res = await getNewSongs()
+		setNewSongs(res.data)
+	}
+	const handleUpcomingSongs = async () => {
+		const res = await getUpcomingSongs()
+		setUpcomingSongs(res.data)
+	}
+
+	//
+
+	const section = (
+		title: string,
+		handleClick: () => void,
+		data: any[],
+		type: string = 'releases'
+	) => {
+		const displayReleases = (data: any[]) => {
+			return (
+				data && (
+					<div className="grid grid-rows-2 grid-flow-col gap-4">
+						{data.map(releaseMap)}
+					</div>
+				)
+			)
+		}
+		const releaseMap = (item: any) => (
+			<a
+				className="flex flex-col basis-auto cols-3"
+				target="_blank"
+				href={`https://music.apple.com/${process.env.STOREFRONT}/album/${item.storeId}`}
+			>
+				<div>
+					<Image
+						src={item.artworkUrl.replace('{w}x{h}', '300x300')}
+						alt={`${item.name} by ${item.artistName}`}
+						width={300}
+						height={300}
+					></Image>
+				</div>
+				<div className="flex flex-col">
+					<div>{item.name}</div>
+					<div>{item.artistName}</div>
+				</div>
+			</a>
+		)
+
+		const displaySongs = (data: any[]) => {
+			return (
+				data && (
+					<div className="flex flex-col basis-auto">
+						{data.map(songsMap)}
+					</div>
+				)
+			)
+		}
+
+		const songsMap = (item: any) => (
+			<div className="flex flex-row">
+				<a
+					className="flex flex-col basis-auto cols-3"
+					target="_blank"
+					href={`https://music.apple.com/${process.env.STOREFRONT}/album/${item.albumId}?i=${item.storeId}`}
+				>
+					<Image
+						src={item.artworkUrl.replace('{w}x{h}', '100x100')}
+						alt={`${item.name} by ${item.artistName}`}
+						width={100}
+						height={100}
+					></Image>
+				</a>
+				<div>{item.name}</div>
+				<div>{item.artistName}</div>
+				<div>{item.albumName}</div>
+				<div>{item.durationInMillis}</div>
+				<div>{item.contentRating}</div>
+			</div>
+		)
+
+		return (
+			<section className="flex flex-col bg-gray-800">
+				<div>
+					<h3>
+						{title}{' '}
+						<button
+							onClick={() => {
+								handleClick()
+							}}
+						>
+							<IoRefreshCircleOutline />
+						</button>
+					</h3>
+				</div>
+
+				{type === 'releases'
+					? displayReleases(data)
+					: displaySongs(data)}
+
+				<div className="grid grid-rows-2 grid-flow-col gap-4">
+					{/* {newReleases &&
+						newReleases.map((item: any) => ( */}
+					{data &&
+						(type === 'releases'
+							? data.map(releaseMap)
+							: data.map(songsMap))}
+				</div>
+			</section>
+		)
+	}
 
 	return (
 		<>
+			{section('New Releases', handleNewReleases, newReleases)}
+			{section('New Singles', handleNewSingles, newSingles)}
+			{section('Upcoming', handleUpcoming, upcoming)}
+			{section('New Songs', handleNewSongs, newSongs, 'tracks')}
+			{section(
+				'Upcoming Songs',
+				handleUpcomingSongs,
+				upcomingSongs,
+				'tracks'
+			)}
+
+			<hr />
 			<div className="flex flex-col gap-4">
 				<button
 					onClick={() => {
--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: '**',
+				// hostname: '**.mzstatic.com',
+				// hostname: 'is1-ssl.mzstatic.com',
+				// hostname: 'is2-ssl.mzstatic.com',
+				// hostname: 'is3-ssl.mzstatic.com',
+			},
+		],
+	},
 	env: {
 		// APP_NAME: process.env.APP_NAME,
 		// APP_BUILD: process.env.APP_BUILD,
 		APP_URL: process.env.APP_URL,
 		DEVELOPER_TOKEN: process.env.DEVELOPER_TOKEN,
+		STOREFRONT: process.env.STOREFRONT,
 		TEST_USER_TOKEN: process.env.TEST_USER_TOKEN,
 	}
 };
