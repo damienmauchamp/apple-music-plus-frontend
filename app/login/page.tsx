20ee83a4bb9555b720c0dcddc05ae62c12436868
Damien Mauchamp <damienmauchamp@gmail.com>
2024-03-12 15:26:52 -0400

    refactoring of login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,30 +1,9 @@
 'use client'
-// import Head from 'next/head'
-// import Link from 'next/link'
-import Label from '@/src/components/Components/label'
-import Input from '@/src/components/Components/input'
-import Button from '@/src/components/Components/button'
-import FormErrors from '@/src/components/Components/formErrors'
-import { ChangeEvent, FormEvent, useState } from 'react'
 import useAuth from '../../lib/useAuth'
-// import { useSearchParams } from 'next/navigation'
+import LoginForm from '@/src/components/Pages/Login/LoginForm'
 
 export default function Login() {
-	// todo : <Suspense></Suspense> when extracting to another component
-	// const searchParams = useSearchParams()
-	// const [email, setEmail] = useState<string>(searchParams.get('email') || '')
-	const [email, setEmail] = useState<string>('')
-	const [password, setPassword] = useState<string>('')
-	const [remember, setRemember] = useState<boolean>(false)
-	const [errors, setErrors] = useState<string[]>([])
-
-	const { login, isLoading, user } = useAuth({ middleware: 'guest' })
-
-	const submitForm = async (event: FormEvent<HTMLFormElement>) => {
-		event.preventDefault()
-
-		login({ email, password, remember, setErrors })
-	}
+	const { isLoading, user } = useAuth({ middleware: 'guest' })
 
 	if (isLoading || user) {
 		return <>Loading...</>
@@ -32,80 +11,9 @@ export default function Login() {
 
 	return (
 		<>
-			{/* <Head>
-				<title>ergodnc — Login</title>
-			</Head> */}
+			<h1>Login</h1>
 
-			<div className={'w-1/2 mx-auto bg-white p-5 rounded-lg'}>
-				<FormErrors className="mb-5" errors={errors} />
-
-				<form onSubmit={(e) => submitForm(e)} autoComplete="off">
-					<div>
-						<Label htmlFor="email">Email</Label>
-						<Input
-							id="email"
-							type="email"
-							value={email}
-							className="block mt-1 w-full"
-							onChange={(event: ChangeEvent<HTMLInputElement>) =>
-								setEmail(event.target.value)
-							}
-							required
-							autoFocus
-							autoComplete="off"
-						/>
-					</div>
-
-					<div className="mt-4">
-						<Label htmlFor="password">Password</Label>
-
-						<Input
-							id="password"
-							type="password"
-							value={password}
-							className="block mt-1 w-full"
-							onChange={(event: ChangeEvent<HTMLInputElement>) =>
-								setPassword(event.target.value)
-							}
-							required
-							autoComplete="current-password"
-						/>
-					</div>
-
-					<div className="block mt-4">
-						<label
-							htmlFor="remember_me"
-							className="inline-flex items-center"
-						>
-							<input
-								id="remember_me"
-								type="checkbox"
-								name="remember"
-								checked={remember}
-								onChange={() =>
-									// event: ChangeEvent<HTMLInputElement>
-									setRemember(!remember)
-								}
-								className="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-							/>
-
-							<span className="ml-2 text-gray-600">
-								Remember me
-							</span>
-						</label>
-					</div>
-
-					<div className="flex items-center justify-end mt-4">
-						{/* <Link href="/forgot-password">
-							<a className="underline text-sm text-gray-600 hover:text-gray-900">
-								Forgot your password?
-							</a>
-						</Link> */}
-
-						<Button className="ml-3">Login</Button>
-					</div>
-				</form>
-			</div>
+			<LoginForm />
 		</>
 	)
 }
---
+++ src/components/Pages/Login/LoginForm.tsx
@@ -0,0 +1,103 @@
+import React, { ChangeEvent, FormEvent, useState } from 'react'
+import { useSearchParams } from 'next/navigation'
+import Label from '@/src/components/Components/Label'
+import Input from '@/src/components/Components/Input'
+import Button from '@/src/components/Components/Button'
+import FormErrors from '@/src/components/Components/FormErrors'
+import useAuth from '@/lib/useAuth'
+
+interface LoginFormProps {
+	onSubmit?: (event: FormEvent<HTMLFormElement>) => void
+	errors?: string[]
+}
+
+const LoginForm = ({ onSubmit, ...props }: LoginFormProps) => {
+	const { login } = useAuth({ middleware: 'guest' })
+
+	const searchParams = useSearchParams()
+	const [email, setEmail] = useState<string>(searchParams.get('email') || '')
+	const [password, setPassword] = useState<string>('')
+	const [remember, setRemember] = useState<boolean>(false)
+	const [errors, setErrors] = useState<string[]>([])
+
+	const submitForm = async (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault()
+
+		login({ email, password, remember, setErrors })
+
+		return onSubmit && onSubmit(event)
+	}
+
+	return (
+		<div className={'w-1/2 mx-auto bg-white p-5 rounded-lg'}>
+			<FormErrors className="mb-5" errors={errors} />
+
+			<form onSubmit={(e) => submitForm(e)} autoComplete="off" {...props}>
+				<div>
+					<Label htmlFor="email">Email</Label>
+					<Input
+						id="email"
+						type="email"
+						value={email}
+						className="block mt-1 w-full"
+						onChange={(event: ChangeEvent<HTMLInputElement>) =>
+							setEmail(event.target.value)
+						}
+						required
+						autoFocus
+						autoComplete="off"
+					/>
+				</div>
+
+				<div className="mt-4">
+					<Label htmlFor="password">Password</Label>
+
+					<Input
+						id="password"
+						type="password"
+						value={password}
+						className="block mt-1 w-full"
+						onChange={(event: ChangeEvent<HTMLInputElement>) =>
+							setPassword(event.target.value)
+						}
+						required
+						autoComplete="current-password"
+					/>
+				</div>
+
+				<div className="block mt-4">
+					<label
+						htmlFor="remember_me"
+						className="inline-flex items-center"
+					>
+						<input
+							id="remember_me"
+							type="checkbox"
+							name="remember"
+							checked={remember}
+							onChange={() =>
+								// event: ChangeEvent<HTMLInputElement>
+								setRemember(!remember)
+							}
+							className="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+						/>
+
+						<span className="ml-2 text-gray-600">Remember me</span>
+					</label>
+				</div>
+
+				<div className="flex items-center justify-end mt-4">
+					{/* <Link href="/forgot-password">
+					<a className="underline text-sm text-gray-600 hover:text-gray-900">
+						Forgot your password?
+					</a>
+				</Link> */}
+
+					<Button className="ml-3">Login</Button>
+				</div>
+			</form>
+		</div>
+	)
+}
+
+export default LoginForm
